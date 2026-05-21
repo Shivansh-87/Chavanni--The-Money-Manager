bee0c40c4efe78804cb536440dcd4d2ff68bf6ad
Shivansh-87 <shivanshh045@gmail.com>
2026-05-21 23:43:18 +0530

    yehh

--- src/main/java/in/MoneyManager/Chavanni/controller/ProfileController.java
+++ src/main/java/in/MoneyManager/Chavanni/controller/ProfileController.java
@@ -5,18 +5,53 @@ import in.MoneyManager.Chavanni.service.ProfileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import in.MoneyManager.Chavanni.dto.AuthDTO;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 public class ProfileController {
 
     public final ProfileService profileService;
+
     @PostMapping("/register")
-    public ResponseEntity<ProfileDTO> registerProfile(@RequestBody ProfileDTO profileDTO){
+    public ResponseEntity<ProfileDTO> registerProfile(@RequestBody ProfileDTO profileDTO) {
         ProfileDTO createdProfile = profileService.registerProfile(profileDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdProfile);
+    }
+
+    @GetMapping("/activate")
+    public ResponseEntity<String> activateProfile(@RequestParam String token) {
+        boolean activated = profileService.activateProfile(token);
+        if (activated) {
+            return ResponseEntity.ok("Profile activated successfully");
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid activation token");
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, Object>> login(@RequestBody AuthDTO authDTO) {
+        try {
+            if (!profileService.isAccountActive(authDTO.getEmail())) {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).body(Map.of(
+                        "message", "Account is not active. Please activate your account first."
+                ));
+            }
+            Map<String, Object> response = profileService.authenticateAndGenerateToken(authDTO);
+            return ResponseEntity.ok(response);
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", e.getMessage()));
+        }
+    }
+    @GetMapping("/test")
+    public String test(){
+        return "Test successful";
     }
 }
--- src/main/java/in/MoneyManager/Chavanni/service/ProfileService.java
+++ src/main/java/in/MoneyManager/Chavanni/service/ProfileService.java
@@ -5,8 +5,16 @@ import in.MoneyManager.Chavanni.entity.ProfileEntity;
 import in.MoneyManager.Chavanni.repository.ProfileRepository;
 import lombok.RequiredArgsConstructor;
 //import org.apache.xmlbeans.impl.xb.xsdschema.Public;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
+import in.MoneyManager.Chavanni.dto.AuthDTO;
+import java.util.Map;
+import in.MoneyManager.Chavanni.util.JwtUtil;
 import java.util.UUID;
 
 @Service
@@ -14,10 +22,15 @@ import java.util.UUID;
 public class ProfileService {
     private final ProfileRepository profileRepository;
     private final EmailService emailService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+
 
     public ProfileDTO registerProfile(ProfileDTO profileDTO){
         ProfileEntity newProfile = toEntity(profileDTO);
         newProfile.setActivationToken(UUID.randomUUID().toString());
+
         newProfile = profileRepository.save(newProfile);
         //send activation link
         String activationLink = "http://localhost:8080/api/v1.0/activate?token="+newProfile.getActivationToken();
@@ -31,7 +44,7 @@ public class ProfileService {
                 .id(profileDTO.getId())
                 .fullName(profileDTO.getFullName())
                 .email(profileDTO.getEmail())
-                .password(profileDTO.getPassword())
+                .password(passwordEncoder.encode(profileDTO.getPassword()))
                 .profileImageUrl(profileDTO.getProfileImageUrl())
                 .createAt(profileDTO.getCreateAt())
                 .updateAt(profileDTO.getUpdateAt())
@@ -54,5 +67,48 @@ public class ProfileService {
             profileRepository.save(profile);
             return true;
         }).orElse(false);
+    }
+
+    public boolean isAccountActive(String email){
+        return profileRepository.findByEmail(email).map(ProfileEntity::getIsActive).orElse(false);
+    }
+
+    public ProfileEntity getCurrentProfile(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return profileRepository.findByEmail(authentication.getName())
+                .orElseThrow(() -> new RuntimeException("Profile not found for email: " + authentication.getName()));
+
+    }
+
+    public ProfileDTO getPublicProfile(String email){
+        ProfileEntity currentUser = null;
+        if(email == null){
+            currentUser = getCurrentProfile();
+        }else{
+            currentUser=profileRepository.findByEmail(email)
+                    .orElseThrow(() -> new UsernameNotFoundException("Profile not found with email: " + email));
+        }
+        return ProfileDTO.builder()
+                .id(currentUser.getId())
+                .fullName(currentUser.getFullName())
+                .email(currentUser.getEmail())
+                .profileImageUrl(currentUser.getProfileImageUrl())
+                .createAt(currentUser.getCreateAt())
+                .updateAt(currentUser.getUpdateAt())
+                .build();
+    }
+
+    public Map<String, Object> authenticateAndGenerateToken(AuthDTO authDTO){
+        try{
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authDTO.getEmail(), authDTO.getPassword()));
+            String token = jwtUtil.generateToken(authDTO.getEmail());
+            return Map.of(
+                    "token", token,
+                    "user",getPublicProfile(authDTO.getEmail())
+            );
+        }catch(Exception e){
+            e.printStackTrace();
+            throw new RuntimeException("Invalid email or password");
+        }
     }
 }
