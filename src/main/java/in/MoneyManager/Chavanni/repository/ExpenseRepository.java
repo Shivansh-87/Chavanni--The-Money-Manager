6a0f522e1fd4a720826ded672d13cacb882712be
Shivansh-87 <shivanshh045@gmail.com>
2026-05-23 11:41:58 +0530

    Done

--- src/main/java/in/MoneyManager/Chavanni/repository/ExpenseRepository.java
+++ src/main/java/in/MoneyManager/Chavanni/repository/ExpenseRepository.java
@@ -3,5 +3,9 @@ package in.MoneyManager.Chavanni.repository;
 import in.MoneyManager.Chavanni.entity.ExpenseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public class ExpenseRepository extends JpaRepository<ExpenseEntity, Long> {
+
+    List<ExpenseEntity> findByProfileId(Long profileId);
 }
